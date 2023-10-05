67a5bfedc7bd3c0bfb8390ec74125e032febc60f
Horatius Nobilis <horatius.nobilis@protonmail.com>
2023-10-05 18:30:19 +0200

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,34 +1,34 @@
-import { SelectField } from "@chakra-ui/react";
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
-import { GameQuery } from "../App";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform } [];
-    metacritic: number;
-  }
-  
-const useGames = (
-  gameQuery: GameQuery
-) => 
-useData<Game>('/games', {
-  params: { 
-    genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id, 
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
-  }}, [
-  gameQuery
-]);
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
+}
+
+const useGames = (gameQuery: GameQuery) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
+      },
+    },
+    [gameQuery]
+  );
 
 export default useGames;
